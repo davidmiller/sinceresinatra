7688a97347cc21f91e203307bcfd9b992c02aeff
David Miller <David>
2012-03-26 18:29:10 +0100

    Differentiate between dev/production environments by hostname

--- myapp.rb
+++ myapp.rb
@@ -7,19 +7,19 @@ require 'sinatra'
 require 'haml'
 
 OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
+scripts = :minscripts
+analyze = true
+if %w[bosch rasputin].member? Socket.gethostname
+  scripts = :devscripts
+  analyze = false
+end
 
 get '/' do
-  scripts = :minscripts
-  analyze = true
-  if %w[bosch rasputin].member? Socket.gethostname
-    scripts = :devscripts
-    analyze = false
-  end
   haml :index, :locals => {:scripts => scripts, :analyze => analyze}
 end
 
 get '/about/' do
-  haml :about
+  haml :about, :locals => {:analyze => analyze}
 end
 
 get '/gitproxy/*' do
