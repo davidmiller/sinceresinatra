f3f250dd45a9748907c99d5b8476e694d8c5dba8
David Miller <David>
2012-10-14 16:34:19 +0100

    Add fabfile for deploymentz

---
+++ fabfile.py
@@ -0,0 +1,13 @@
+from fabric.api import *
+
+web = ['happenup@deadpansincerity.com']
+
+@hosts(web)
+def deploy():
+    """
+    Pull changes and restart
+    """
+    with cd("/home/happenup/webapps/rubysincerity/sincere"):
+        run("git pull github master")
+    with cd("/home/happenup/webapps/rubysincerity"):
+        run("./bin/restart")
